ff251953320770b8d4c148c2d00090a27356fa2d
Charles Drake <cdrake1205@gmail.com>
2024-02-05 14:36:20 -0500

    added a shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("user", function(name, githubUsername){
+    return `<div class="user">
+  <div class="user_name">${name}</div>
+  <div class="user_github">@${githubUsername}</div></div>`;});
 };
